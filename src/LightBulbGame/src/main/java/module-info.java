af070e9214ffcef058e028f1b1f5c60159d2de83
Bedřich Petrášek <62395378+PetrasekBedrich@users.noreply.github.com>
2025-05-03 15:39:39 +0200

    Delete src/LightBulbGame directory

--- src/LightBulbGame/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module ija.project.lightbulbgame {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens ija.project.lightbulbgame to javafx.fxml;
-    exports ija.project.lightbulbgame;
-}
